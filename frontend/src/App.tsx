e31953bf1ffadd29eb6c6693f4d59e2729ff8f56
Pedro Rojas <pedro.rojasm3@userena.cl>
2025-11-21 23:34:05 -0300

    reinicio de frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import HomePage from './pages/HomePage'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>Transparencia</h1>
-
-    <div className='cards-holder'>
-      <div className='card'>
-        <h2>Gastos</h2>
-        <p>Veja os gastos detalhados do governo.</p>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-
-      <div className='card'>
-        <h2>Receitas</h2>
-        <p>Confira as fontes de receita do governo.</p>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
       </div>
-
-      <div className='card'>
-        <h2>Contratos</h2>
-        <p>Acesse os contratos firmados pelo governo.</p>
-      </div>
-    </div>
-    <HomePage />
-
-
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
--- frontend/src/config.ts
+++
@@ -1,1 +0,0 @@
-export const API_BASE_URL = import.meta.env.VITE_API_URL || "http://3.200.55.100:8000";
